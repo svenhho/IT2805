cea70324b27cd1d04c0fddb5b808696ee18b2f1f
odatill <odat_1998@live.no>
2020-10-29 19:55:52 +0100

    Oda

--- P3/KLPP/modularity/menybar.js
+++ P3/KLPP/modularity/menybar.js
@@ -18,6 +18,7 @@ function menybar() {
 
     const bestill_time = document.createElement("a");
     const bestill_time_tekst = document.createTextNode("BESTILL TIME");
+    bestill_time.id = "bestill_time_id";
     bestill_time.href= "bestill_time.html";
     bestill_time.appendChild(bestill_time_tekst);
     meny.appendChild(bestill_time);
@@ -25,30 +26,35 @@ function menybar() {
     const behandlinger = document.createElement("a");
     const behanlinger_tekst = document.createTextNode("BEHANDLINGER");
     behandlinger.href= "behandlinger.html";
+    behandlinger.id = "behandlinger_id";
     behandlinger.appendChild(behanlinger_tekst);
     meny.appendChild(behandlinger);
 
     const om_oss = document.createElement("a");
     const om_oss_tekst = document.createTextNode("OM OSS");
     om_oss.href= "om_oss.html";
+    om_oss.id = "om_oss_id";
     om_oss.appendChild(om_oss_tekst);
     meny.appendChild(om_oss);
 
     const søk_jobb = document.createElement("a");
     const søk_jobb_tekst = document.createTextNode("SØK JOBB");
     søk_jobb.href= "søk_jobb.html";
+    søk_jobb.id = "søk_jobb_id";
     søk_jobb.appendChild(søk_jobb_tekst);
     meny.appendChild(søk_jobb);
 
     const galleri = document.createElement("a");
     const galler_tekst = document.createTextNode("GALLERI");
     galleri.href= "galleri.html";
+    galleri.id = "galleri_id";
     galleri.appendChild(galler_tekst);
     meny.appendChild(galleri);
 
     const covid_19 = document.createElement("a");
     const covid_19_tekst = document.createTextNode("COVID-19");
     covid_19.href= "covid_19.html";
+    covid_19.id = "covid_19_id";
     covid_19.appendChild(covid_19_tekst);
     meny.appendChild(covid_19);
 }
